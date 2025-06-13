3637738e1263d72d9249e961c878defc223263f5
Sniper0141 <116571645+Sniper0141@users.noreply.github.com>
2025-06-13 15:56:50 +0200

    Person implements Comparable

--- src/main/java/ch/bbw/ss/Model/Person.java
+++ src/main/java/ch/bbw/ss/Model/Person.java
@@ -1,7 +1,14 @@
 package ch.bbw.ss.Model;
 
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 
-public record Person(String name, Contact contact, Location location, Date birthDate, List<Contact> friends) {
+public record Person(String name, Contact contact, Location location, Date birthDate, List<Contact> friends)
+        implements Comparable {
+    @Override
+    public int compareTo(Object o) {
+        var p = (Person)o;
+        return birthDate.compareTo(p.birthDate);
+    }
 }
